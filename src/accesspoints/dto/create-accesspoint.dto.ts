da10010edd63dabb21b5099d465abc7a9c7bb9da
Saksorn Malasuttichai <s.malasutt@gmail.com>
2025-07-13 20:47:20 +0700

    ap create-dto

--- src/accesspoints/dto/create-accesspoint.dto.ts
+++ src/accesspoints/dto/create-accesspoint.dto.ts
@@ -1,1 +1,11 @@
-export class CreateAccesspointDto {}
+export class CreateAccesspointDto {
+  ip: string;
+  radMac: string;
+  ethMac: string;
+  name: string;
+  location: string;
+  model: string;
+  ios: string;
+  serial: string;
+  problem?: string;
+}
