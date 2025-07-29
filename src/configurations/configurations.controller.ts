903fbc8baa773963a6d3814ca7266c4c5f2323f6
Saksorn Malasuttichai <s.malasutt@gmail.com>
2025-07-29 10:27:16 +0700

    upload multiple file

--- src/configurations/configurations.controller.ts
+++ src/configurations/configurations.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ConfigurationsService } from './configurations.service';
 import { CreateConfigurationDto } from './dto/create-configuration.dto';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 
 @Controller('configurations')
 export class ConfigurationsController {
@@ -12,7 +23,47 @@ export class ConfigurationsController {
   }
 
   @Post('create')
-  create(@Body() createConfigurationDto: CreateConfigurationDto) {
+  create(
+    @Body()
+    createConfigurationDto: CreateConfigurationDto,
+  ) {
     return this.configurationsService.create(createConfigurationDto);
+  }
+
+  @Post('edit/:id')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'ap', maxCount: 1 },
+        { name: 'location', maxCount: 1 },
+      ],
+      {
+        storage: memoryStorage(),
+        fileFilter(req, file, callback) {
+          // allow not uploading file
+          if (!file) {
+            return callback(null, true);
+          }
+          const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+          if (!allowedTypes.includes(file.mimetype)) {
+            return callback(
+              new BadRequestException(
+                'File type not matched with jpeg, png, or gif',
+              ),
+              false,
+            );
+          }
+          callback(null, true);
+        },
+      },
+    ),
+  )
+  edit(
+    @UploadedFiles()
+    files: { ap?: Express.Multer.File[]; location?: Express.Multer.File[] },
+    @Body()
+    createConfigurationDto: { name: string; buildingId: number; ip: string },
+  ) {
+    console.dir({ files, createConfigurationDto }, { depth: null });
   }
 }
--- src/configurations/configurations.module.ts
+++ src/configurations/configurations.module.ts
@@ -5,12 +5,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Configuration } from './entities/configuration.entity';
 import { IpModule } from '../ip/ip.module';
 import { LocationsModule } from '../locations/locations.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Configuration]),
     IpModule,
     LocationsModule,
+    MulterModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dest: join(
+          process.cwd(),
+          configService.get<string>('UPLOAD_DIR', 'uploads'),
+          'configurations',
+        ),
+        limits: {
+          fileSize: 10 * 1024 * 1024, // 10 MB
+        },
+      }),
+    }),
   ],
   controllers: [ConfigurationsController],
   providers: [ConfigurationsService],
